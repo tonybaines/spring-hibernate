3c7c1587880ece1ef5847abe24bd064b1f6205d0
Tony Baines <tony.baines.1@gmail.com>
2011-02-01 22:16:20 +0000

    Working JSON response using the Jackson library

--- src/main/java/masterclass/spring/controllers/BookController.java
+++ src/main/java/masterclass/spring/controllers/BookController.java
@@ -1,18 +1,24 @@
 package masterclass.spring.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
 
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public String getBook(@PathVariable("id") long id, Model model) {
-    return "books";
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET, headers = {"Accept=application/json"})
+  public @ResponseBody Map getBook(@PathVariable("id") long id, Model model) {
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("Greeting", "Hello world");
+    return map;
   }
 
 }
