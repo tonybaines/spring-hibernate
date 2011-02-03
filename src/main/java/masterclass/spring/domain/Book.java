26e7d6f4a5df0fd15c280c51fbd615808c16b269
802661379 <tony.baines@bt.com>
2011-02-03 14:04:33 +0000

    Sync with Marcus from SVN

--- src/main/java/masterclass/spring/domain/Book.java
+++ src/main/java/masterclass/spring/domain/Book.java
@@ -1,9 +1,26 @@
 package masterclass.spring.domain;
 
-public class Book {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-  public long getId() {
-    return 1;
-  }
+
+@Entity
+@Table(name="BOOKS")
+public class Book {
+	
+	
+	@Id
+    @Column(name="ID")
+    @GeneratedValue
+    private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+	
+	
 
 }
