8c12bc06e2b4b1857a01353a821b23feb1969b17
Tony Baines <tony.baines.1@gmail.com>
2011-02-05 15:02:05 +0000

    Further exercises

--- exercise2/src/Main.java
+++ exercise2/src/Main.java
@@ -12,13 +12,20 @@ public class Main {
     Deliverable javaDeliverable = new SimpleDeliverable(javaDeveloper);
     List<Deliverable> deliverables = new ArrayList<Deliverable>();
     deliverables.add(javaDeliverable);
-    Project simpleProject = new Project(deliverables);
+    Project simpleJavaProject = new Project(deliverables);
 
     //
-    // TODO: Introduce a ProjectBuilder so that the next line works
-    // Project simpleProject = new ProjectBuilder().getSimpleProject();
+    // TODO: Introduce a ProjectBuilder so that the next line works 
+    //       (hint: just move the code above into the new class)
+    // Project simpleJavaProject = new ProjectBuilder().getBean("simpleJavaProject"); 
     
-    System.out.println(simpleProject.deliver());
+    System.out.println(simpleJavaProject.deliver());
+    
+    //
+    // TODO: Extend the builder to assemble a variety of different projects
+    //       e.g. complexJavaProject, complexMultiTechnologyProject, ...
+    //       N.B. Make sure that developers can be reused between projects
+    
   }
 
 }
