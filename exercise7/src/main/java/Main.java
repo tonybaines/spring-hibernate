04612368b0e56c38356d7f1f466e8808ef0c119f
Tony Baines <tony.baines.1@gmail.com>
2011-02-06 15:15:23 +0000

    No XML config file needed any more

--- exercise7/src/main/java/Main.java
+++ exercise7/src/main/java/Main.java
@@ -1,14 +1,15 @@
 import masterclass.spring.annotations.Car;
+import masterclass.spring.annotations.MyApplicationContext;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class Main {
   public static void main(String[] args) {
     
-    // The Spring definition file is in src/main/resources
-    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    // No XML!
+    ApplicationContext context = new AnnotationConfigApplicationContext(MyApplicationContext.class);
     context.getBean(Car.class);
   }
 
